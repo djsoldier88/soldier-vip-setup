912a6768cd8bdbac787e5f91d797b2f48e8e3337
djsoldier88 <lilyo.links@gmail.com>
2026-07-23 17:38:37 +0300

    Link setup page to release installers

--- downloads.js
+++ downloads.js
@@ -1,5 +1,5 @@
 // Replace the empty strings with the current direct file links before publish.
 window.SOLDIER_VIP_DOWNLOADS = {
-  windows: "",
-  macos: "",
+  windows: "https://github.com/djsoldier88/soldier-vip-setup/releases/download/v0.2.1/Soldier-VIP-Setup-0.2.0.exe",
+  macos: "https://github.com/djsoldier88/soldier-vip-setup/releases/download/v0.2.1/Soldier-VIP-macOS-apple-silicon.zip",
 };
